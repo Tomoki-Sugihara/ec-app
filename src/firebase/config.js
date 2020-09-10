e5bca9cfb343bf80e0dfc4498ff72b599d1f549b
sugihara10969 <sugihara.10969@gmail.com>
2020-09-10 11:36:49 +0900

    [add]firestore-config

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyA1khqNvqo5N4yCIRtVTlaQcLTuDRfP6vg",
+   authDomain: "ec-app-b4eb8.firebaseapp.com",
+   databaseURL: "https://ec-app-b4eb8.firebaseio.com",
+   projectId: "ec-app-b4eb8",
+   storageBucket: "ec-app-b4eb8.appspot.com",
+   messagingSenderId: "704945772108",
+   appId: "1:704945772108:web:b33f335463df9abbe386e9",
+   measurementId: "G-63EJX631L8"
+ };
