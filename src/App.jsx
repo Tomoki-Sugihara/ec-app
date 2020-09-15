c627a56f8fcc985d5ad8b67b54c10af0f241c8b1
sugihara10969 <sugihara.10969@gmail.com>
2020-09-15 12:47:58 +0900

    [add]reactとreduxの接続テスト

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './assets/reset.css';
+import './assets/style.css';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { signInAction } from './reducks/users/actions';
+
+import Router from './Router';
 
 function App() {
-  return (
-    <div>hello</div>
-  );
+   // const dispatch = useDispatch();
+   // const selector = useSelector(state => state);
+   // console.log(selector.users);
+   return (
+      <>
+         <main className="c-main">
+            <Router />
+         </main>
+      </>
+   );
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import * as History from 'history';
+import createStore from './reducks/store/store';
+// import {ErrorBoundary} from "./components/UIkit";
+// import { MuiThemeProvider } from '@material-ui/core/styles';
+// import { theme } from './assets/theme';
+import * as serviceWorker from './serviceWorker';
+
 import App from './App';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+// connected-react-router - action経由でルーティングが可能、push,replace..
+// historyを強化
+const history = History.createBrowserHistory();
+export const store = createStore(history);
 
+render(
+   //  <Provider store={store}>
+   //     <ConnectedRouter history={history}>
+   //        <MuiThemeProvider theme={theme}>
+   //           <ErrorBoundary>
+   //              <App />
+   //           </ErrorBoundary>
+   //        </MuiThemeProvider>
+   //     </ConnectedRouter>
+   //  </Provider>,
+   <Provider store={store}>
+      <ConnectedRouter history={history}>
+         <App />
+      </ConnectedRouter>
+   </Provider>,
+   document.getElementById('root')
+);
+serviceWorker.register();
